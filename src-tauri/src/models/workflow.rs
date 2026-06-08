186520d0d8c2bfc708776148e6e81ce1ee9b9117
VISHESH SACHAN <visheshsachan021@gmail.com>
2026-06-08 22:04:47 +0530

    refactor: remodel workflow steps

--- src-tauri/src/models/workflow.rs
+++ src-tauri/src/models/workflow.rs
@@ -1,88 +1,224 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Default)]
+pub struct WorkflowContext {
+    pub variables: HashMap<String, String>,
+}
+
+impl WorkflowContext {
+    pub fn get(&self, key: &str) -> Option<&String> {
+        self.variables.get(key)
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.variables.insert(key, value);
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Workflow {
-    pub id: String,
-    pub name: String,
-    pub steps: Vec<WorkflowStep>,
+    pub steps: Vec<Step>,
+    pub flow: Flow,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct WorkflowStep {
-    pub id: String,
-    pub name: String,
-    pub step_type: StepType,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum StepType {
-    Check(CheckStep),
+#[serde(tag = "type")]
+pub enum Step {
     Input(InputStep),
-    Command(CommandStep),
-    File(FileStep),
-    Choice(ChoiceStep),
-    Condition(ConditionStep),
     Information(InformationStep),
+    Check(CheckStep),
+    Condition(ConditionStep),
+    Command(CommandStep),
+    Choice(ChoiceStep),
+    File(FileStep),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct CheckStep {
-    pub check_type: CheckType,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum CheckType {
-    ExecutableExists,
-    FileExists,
-    DirectoryExists,
-    ProcessRunning,
-}
+//
+// INPUT
+//
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct InputStep {
-    pub key: String,
-    pub label: String,
+    pub id: String,
+    pub name: String,
+    pub variable_name: String,
+    pub prompt: String,
+
+    pub description: Option<String>,
+    pub default_value: Option<String>,
     pub required: bool,
     pub secret: bool,
-    pub default_value: Option<String>,
+    pub validation_regex: Option<String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct CommandStep {
-    pub command: String,
-    pub working_directory: Option<String>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct FileStep {
-    pub action: FileAction,
-    pub target_path: String,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum FileAction {
-    Create,
-    Copy,
-    Append,
-    Replace,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ChoiceStep {
-    pub key: String,
-    pub label: String,
-    pub options: Vec<String>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ConditionStep {
-    pub variable: String,
-    pub expected_value: String,
-}
+//
+// INFORMATION
+//
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct InformationStep {
+    pub id: String,
+    pub name: String,
     pub title: String,
-    pub message: String,
-    pub link: Option<String>,
+    pub content: String,
+}
+
+//
+// CHECK
+//
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CheckStep {
+    pub id: String,
+    pub name: String,
+    pub check_type: CheckType,
+
+    pub on_success: Option<Workflow>,
+    pub on_failure: Option<Workflow>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum CheckType {
+    Command(CommandCheck),
+    FileExists(FileExistsCheck),
+    DirectoryExists(DirectoryExistsCheck),
+    EnvironmentVariable(EnvironmentVariableCheck),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CommandCheck {
+    pub id: String,
+    pub command: String,
+    pub expected_exit_code: i32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FileExistsCheck {
+    pub id: String,
+    pub path: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DirectoryExistsCheck {
+    pub id: String,
+    pub path: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EnvironmentVariableCheck {
+    pub id: String,
+    pub variable_name: String,
+}
+
+//
+// CONDITION
+//
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ConditionStep {
+    pub id: String,
+    pub name: String,
+
+    pub left: String,
+    pub operator: ConditionOperator,
+    pub right: String,
+
+    pub on_true: Option<Workflow>,
+    pub on_false: Option<Workflow>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum ConditionOperator {
+    Equals,
+    NotEquals,
+    GreaterThan,
+    GreaterThanOrEqual,
+    LessThan,
+    LessThanOrEqual,
+    Contains,
+}
+
+//
+// COMMAND
+//
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CommandStep {
+    pub id: String,
+    pub name: String,
+    pub command: String,
+
+    pub working_directory: Option<String>,
+
+    pub capture_stdout_to: Option<String>,
+    pub capture_stderr_to: Option<String>,
+    pub capture_exit_code_to: Option<String>,
+
+    pub on_success: Option<Workflow>,
+    pub on_failure: Option<Workflow>,
+}
+
+//
+// CHOICE
+//
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ChoiceStep {
+    pub id: String,
+    pub name: String,
+
+    pub variable_name: String,
+    pub prompt: String,
+
+    pub options: Vec<String>,
+
+    pub default_value: Option<String>,
+    pub allow_custom_value: bool,
+}
+
+//
+// FILE
+//
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FileStep {
+    pub id: String,
+    pub name: String,
+
+    pub operation: FileOperation,
+    pub file_path: String,
+
+    pub on_success: Option<Workflow>,
+    pub on_failure: Option<Workflow>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum FileOperation {
+    CreateOrOverwrite {
+        content: String,
+    },
+
+    Append {
+        content: String,
+    },
+
+    ReplaceText {
+        search: String,
+        replace: String,
+    },
+}
+
+//
+// JUMP
+//
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Flow {
+    Continue,
+    Jump {
+        target_step_id: String,
+    },
 }
