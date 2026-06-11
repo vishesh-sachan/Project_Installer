ed6f9e2e305c07a1b5cf97ce7abc9f360af7b29b
VISHESH SACHAN <visheshsachan021@gmail.com>
2026-06-11 15:00:22 +0530

    feat: connect pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,82 @@
+import { useState } from "react";
+import ProjectSelectionPage from "./pages/ProjectSelectionPage";
+import ProjectOverviewPage from "./pages/ProjectOverviewPage";
 import WorkflowBuilder from "./pages/WorkflowBuilder";
 
+type AppState =
+  | {
+      page: "projectSelection";
+    }
+  | {
+      page: "projectOverview";
+      projectPath: string;
+    }
+  | {
+      page: "workflowBuilder";
+      projectPath: string;
+      workflowId: string;
+    };
+
 function App() {
-  return <div>
-      <WorkflowBuilder />
-    </div>
+  const [state, setState] =
+    useState<AppState>({
+      page: "projectSelection",
+    });
+
+  switch (state.page) {
+    case "projectSelection":
+      return (
+        <ProjectSelectionPage
+          onProjectSelected={(
+            projectPath
+          ) =>
+            setState({
+              page:
+                "projectOverview",
+              projectPath,
+            })
+          }
+        />
+      );
+
+    case "projectOverview":
+      return (
+        <ProjectOverviewPage
+          projectPath={
+            state.projectPath
+          }
+          onBack={() =>
+            setState({
+              page:
+                "projectSelection",
+            })
+          }
+          onOpenEditor={(
+            workflowId
+          ) =>
+            setState({
+              page:
+                "workflowBuilder",
+              projectPath:
+                state.projectPath,
+              workflowId,
+            })
+          }
+        />
+      );
+
+    case "workflowBuilder":
+      return (
+        <WorkflowBuilder
+          projectPath={
+            state.projectPath
+          }
+          workflowId={
+            state.workflowId
+          }
+        />
+      );
+  }
 }
 
 export default App;
